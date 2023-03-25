1a6e5e15758fb29f8482d6e47db72af5cf7ca08e
Kateryna Skepska <118077803+wonderotter@users.noreply.github.com>
2023-03-25 21:38:40 +0200

    11

--- index.js
+++ index.js
@@ -1,5 +1,133 @@
-if (10 > 15) {
-  console.log("hello its true");
-} else {
-       console.log("hello its false");
-}
+// if (10 > 15) {
+//   console.log("hello its true");
+// } else {
+//   console.log("hello its false");
+// }
+
+// const time = 10;
+
+// if (time < 12) {
+//   console.log('Good morning');
+// } else if (time >= 12 && time < 18) {
+//   console.log('Good day');
+// } else {
+//   console.log('Good evening');
+// }
+
+// (time < 12) ? console.log('Good morning') : console.log('Good day');
+
+
+// let userName;
+// let userAge = 20;
+// if (userAge > 18) {
+//   userName = 'Adult';
+// } else {
+//   userName = 'Kid';
+// }
+// console.log(userName)
+
+
+
+// let userAge = 5;
+// let userName = userAge > 18 ? 'Adult' : 'Kid';
+// console.log(userName);
+
+
+// let greeting = 'Hello dog, ';
+// let howAreYou = 'How are you?';
+
+// let sayHi = greeting + howAreYou;
+
+// let sayHi = `${greeting}${howAreYou}`
+// console.log(sayHi);
+
+
+
+// function sayHi() {
+//   console.log('Hello dear cute user!');
+// }
+
+
+// const sayHi = function () {
+//   console.log('Hello dear cute user!');
+// }
+
+// sayHi()
+
+
+// function sayHi(userName) {
+//   console.log(`hello, ${userName} how are you?`)
+// }
+
+// sayHi('Dillan')
+// sayHi('Allen')
+// sayHi('Dex')
+
+
+// function sum(a, b) {
+//   const result = a * b;
+//   return result
+// console.log(result)
+// }
+
+// let rex = sum(600, 565);
+// console.log(rex);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// function doSmth(test) {
+//   let x = 34;
+//   let y = 15;
+//   let result = test(x, y)
+//   console.log(result);
+// }
+
+// doSmth(sum);
+
+
+// function sayHi(name) {
+//   console.log(`Hello, ${name}! How are you?`);
+// };
+
+// sayHi('Dog');
+
+// const arrowHi = (name) => {
+//   console.log(`Hello, ${name}! How are you?`);
+// };
+
+// arrowHi('Cat');
+
+// function summ(a, b) {
+//   const result = a + b;
+// }
+
+// const summ = (a, b) => a + b;
+// const res = summ(16, 45);
+// console.log(res);
+
+
+// const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+// console.log(autoBrands[3]);
+// console.log(autoBrands.length);
+
+// push - add in the end
+// const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+// autoBrands.push('Nissan')
+// console.log(autoBrands);
+
+// splice delete elem [start, deleteCount]
+// const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+// autoBrands.splice(2, 2)
+// console.log(autoBrands);
+
+// for (let item of autoBrands) {
+//   console.log(item);
+// }
+
+const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+
+autoBrands.forEach(function (item, index) {
+  console.log(`${item} => ${index}`);
+})
