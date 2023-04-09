965e36012447d05109fd36d03e96c723727f8f7c
Kateryna Skepska <118077803+wonderotter@users.noreply.github.com>
2023-04-09 11:37:34 +0300

    11

--- index.js
+++ index.js
@@ -168,7 +168,21 @@
 // }
 // console.log(palindrom('hello'));
 
-const d = 15;
-console.log(d);
+// const d = 15;
+// console.log(d);
+
+
+// const title = 'Top 10 benefits of React framework';
+// const normalizedTitle = title.toLowerCase();
+// console.log(normalizedTitle);
+
+// const words = normalizedTitle.split(' ');
+// console.log(words);
+
+// const slug = words.join('-');
+// console.log(slug);
+
+// const slug1 = title.toLowerCase().split(' ').join('-');
+// console.log(slug1);
 
 
